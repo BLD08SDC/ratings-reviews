8ed54f973237f21787a3e16b977d5ddcf7e8c5a4
thomHayner <thom.hayner@gmail.com>
2020-04-15 00:32:19 -0600

    Fix port Variable, Fix Comments, Fix MVC Model

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,9 @@
+// This file should be clean, containing only the server and API routes as pure functions
+
+// This imports the dotenv dependency (for setting server port and other secret values)
 require('dotenv').config();
+
+// This imports new relic, a dependency that helps with testing
 require('newrelic'); 
 
 // import Express
@@ -9,7 +14,8 @@ const queryPool = require('./queries');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+// This sets a port variable equal to the dotenv PORT value
+const port = process.env.PORT;
 
 // These app.use statements tell Express which middleware 'layers' to apply
 app.use(express.json());
@@ -22,61 +28,36 @@ app.use(
 // This is the API route for getting a list of reviews (currently returns duplicates if review has more than one photo attached to it)
 app.get('/reviews/:product_id/list', (req, res) => {
     queryPool.getListOfReviews(req.params)
-            .then(data => {
-                const results = data.rows.map(i => ({
-                    "review_id": i.id,
-                    "rating": i.rating,
-                    "summary": i.summary,
-                    "recommend": i.recommend,
-                    "response": i.response,
-                    "body": i.body,
-                    "date": i.date,
-                    "reviewer_name": i.reviewer_name,
-                    "helpfulness": i.helpfulness,
-                    "photos": [i.url],
-                }));
-                
-                return res.send(
-                    {
-                        "product": req.params.product_id,
-                        "page": 0,
-                        "count": 5,
-                        "results": results,
-                    }
-            )})
-            .catch(error => console.log(error))
+      .then(data => res.send(data))
+      .catch(() => res.sendStatus(400))
 })
 
 // This is the API route for getting the characteristics meta data (incomplete / currently returns nothing)
 app.get('reviews/:product_id/meta', (req, res) => {
     queryPool.getCharacteristicsMeta(req.params)
-        .then(res.send(
-            {
-
-            }
-        ))
-        .catch(error => console.log(error))
+      .then(data => res.send(data))
+      .catch(() => res.sendStatus(400))
 })
 
 // This is the API route for adding a new review (does not currently handle adding a photo)
 app.post('/reviews/:product_id', (req, res) => {
     queryPool.addReview(req.params)
-        .then(res.send("Thank\'s for your review!"))
-        .catch(error => console.log(error))
+      .then(res.send("Thank\'s for your review!"))
+      .catch(() => res.sendStatus(400))
 })
 
 // This is the API route for marking a review as helpful
 app.put('/reviews/helpful/:review_id', (req, res) => {
     queryPool.markHelpful(req.params)
-        .then(res.send('Thank\'s for the feedback'))
-        .catch(error => console.log(error))
+      .then(res.send('Thank\'s for the feedback'))
+      .catch(() => res.sendStatus(400))
 })
 
 // This is the API route for reporting and review
 app.put('/reviews/report/:review_id', (req, res) => {
     queryPool.reportReview(req.params)
-        .then(res.send('Review Reported'))
-        .catch(error => console.log(error))
+      .then(res.send('Review Reported'))
+      .catch(() => res.sendStatus(400))
 })
 
 // Express needs to know what port to listen on, a console.log callback serves as a confirmation that the server is properly functioning and running
